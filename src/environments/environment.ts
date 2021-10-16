3e24ecf4ced710b2c5899b6cb9b6cc4fea668943
kousikaganesan <g.kousika@gmail.com>
2021-10-17 01:38:02 +0530

    Fixed client id issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: false,
-  spotifyClientId: 'https://accounts.spotify.com/authorize/?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=http://localhost:4200/callback&scope=user-read-private%20user-read-email'
+  spotifyClientId: 'https://accounts.spotify.com/authorize/?client_id=4fc46c474434406bac1ee981bd6785d5&response_type=code&redirect_uri=http://localhost:4200/callback&scope=user-read-private%20user-read-email'
 };
-
-// http://localhost:4200/
-// callback?
-// code=AQDFYdr9Ztug8DilQKC_Pht3cDMLaYR-3-ODBb_tWQ9b6aXyeBQ_wgjxvAGNI4ZH3vLpttFepismQuswnmd_DXnzRNGfYpVunRzoGwgwDpRMlCpvuwDhu6IVnP_aHutOaaK2nLnWsoRyBHqjV35AV-qZTOKhjg2FVumlPXq16EhWVvc242-2DIPRTmsAY-ZHzk_RwONJjUf-8bPjDTb_BHRzUYQQoQ
